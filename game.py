3107b89d9494b365af777c424db1fd932badc370
CalvinYan <calviny524@gmail.com>
2019-04-30 14:15:07 -0700

    Add minimax search algorithm, performance unsatisfactory

--- game.py
+++ game.py
@@ -1,15 +1,13 @@
 import numpy as np
 import random
-import math
 
 # Depth of the minimax tree - how many turns the CPU should look ahead before making their move
-depth = 2
+MAX_DEPTH = 2
 
 # The grid in which the game is played
 board = np.array([["*" for col in range(0, 7)] for row in range(0, 6)])
 # Receive/validate player input
 def playerTurn():
-    global board
 
     col = input("Select a column to place your piece in (1-7): ")
     
@@ -22,14 +20,44 @@ def playerTurn():
     if col < 0 or col > 6:
         print("Column must be between 1 and 7!")
         return playerTurn()
-    if board[0][col] != "*":
+    if isColumnFilled(board, col):
         print("The col you chose is full!")
         return playerTurn()
     return col
 
+# Initiate the minimax algorithm and return its result
+def cpuTurn():
+    return minimaxSearch(board, 0)[0]
+
+# Where the magic happens
+def minimaxSearch(board, depth):
+    if depth == MAX_DEPTH:
+        return (-1, heuristic(board))
+    # Loop over each possible move
+    best_score = -100000000 # The best hypothetical outcome that any move could produce
+    best_move = 0 # The move that produces this best outcome
+    for col in range(0, 7):
+        if not isColumnFilled(board, col):
+            new_board = np.array(board)
+            placePiece(new_board, col, "X")
+            # Odd depths represent the player's turn; they want the heuristic to be as low as possible,
+            # so we invert it
+            new_score = (-1 ** depth) * minimaxSearch(new_board, depth + 1)[1]
+            if (new_score > best_score):
+                best_score = new_score
+                best_move = col
+    return (best_move, best_score)
+    
+
+    # Even depths represent the CPU's turn, so we want the move that leads to the highest heuristic
+
+    # Odd depths represent the player's turn, so we want the move that leads to the lowest heuristic
+         
+
+
 # Update the board with the new piece's position and return this position
 # The value of team is 0 for player, 1 for CPU
-def placePiece(col, piece):
+def placePiece(board, col, piece):
     row = 5
     while board[row][col] != "*":
         row -= 1
@@ -47,7 +75,7 @@ def placePiece(col, piece):
 # The capital X's horizontal chain is length 2; its vertical chain is length 1; its positive chain is
 # length 4; and its negative chain is length 3. We know that if victory is achieved, it must have
 # been achieved using the most recent piece. Thus we search for a chain from this piece at least 4 long.
-def checkWin(row, col):
+def checkWin(board, row, col):
     piece = board[row][col]
     if piece == "*": return False # Shouldn't happen but just as a precaution
 
@@ -74,53 +102,52 @@ def checkWin(row, col):
             searchCol += direction[1]
 
     for length in lengths:
-        # print(length)
         if length >= 4: 
             return True
     return False
 
-# Heuristic function representing a player's advantage
-# Calculated by summing the squares of the lengths of each continguous chain of pieces owned by the player
+# Heuristic function representing the CPU's score advantage over the player
+def heuristic(board):
+    return score(board, "X") - score(board, "O")
+
+# The squared sum of the lengths of each continguous chain of pieces owned by a player, representing their
+# board presence
 def score(board, player):
     score = 0
     # Check rows
     for row in board:
         score += square_sum_chains(row, player)
-    # print(score)
 
     # Check columns
     for col in range(0, 7):
         score += square_sum_chains(board[:, col], player)
-    # print(score)
 
     # Check negative slope diagonals
     # Diagonals constrained by column
     for col in range(1, 7):
-        # print([board[i - col, i] for i in range(col, 7)])
         score += square_sum_chains([board[i - col, i] for i in range(col, 7)], player)
     # Diagonals constrained by row
     for row in range(0, 6):
-        # print([board[i, i - row] for i in range(row, 6)])
         score += square_sum_chains([board[i, i - row] for i in range(row, 6)], player)
-    # print(score)
 
     # Check positive slope diagonals
     for col in range(0, 6):
-        # print([board[col - i, i] for i in range(col, -1, -1)])
         score += square_sum_chains([board[col - i, i] for i in range(col, -1, -1)], player)
     for row in range(0, 6):
-        # print([board[i, 6 - i + row] for i in range(row, 6)])
         score += square_sum_chains([board[i, 6 - i + row] for i in range(row, 6)], player)
-    # print(score)
+    
     return score
 
 # Display the board with pretty formatting
-def printBoard():
+def printBoard(board):
     print ("  1 2 3 4 5 6 7")
     for index, row in enumerate(board):
         print(str(index+1) + " " + " ".join(row))
     print("o" + str(score(board, "O")))
     print("x" + str(score(board, "X")))
+
+def isColumnFilled(board, col):
+    return board[0][col] != "*"
 
 # Utility bounds-checking function
 def inBounds(row, col):
@@ -139,29 +166,32 @@ def square_sum_chains(row, player):
             # Detect end of row
             if index == len(row) - 1:
                 length = index - start_index + 1
-                score += length ** 2
+                score += length ** 3
+                if (length >= 4): score += 10000 # Heuristic heavily favors wins
         if value != player:
             # Detect end of chain
             if index > 0 and row[index-1] == player:
                 length = index - start_index
-                score += length ** 2
+                score += length ** 3
+                if (length >= 4): score += 10000
 
     return score
 
 while True:
     col = playerTurn()
-    row, col = placePiece(col, "O")
+    row, col = placePiece(board, col, "O")
     
-    if checkWin(row, col):
-        printBoard()
+    if checkWin(board, row, col):
+        printBoard(board)
         print("O wins!")
         quit()
     
-    col = random.randint(0, 6)
-    while board[0][col] != "*": col = random.randint(0, 6)
-    row, col = placePiece(col, "X")
-    printBoard()
-    if checkWin(row, col):
+    col = cpuTurn()
+    # col = random.randint(0, 6)
+    # while isColumnFilled(board, col): col = random.randint(0, 6)
+    row, col = placePiece(board, col, "X")
+    printBoard(board)
+    if checkWin(board, row, col):
         print("X wins!")
         quit()
 
